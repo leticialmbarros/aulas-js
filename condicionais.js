b8b427037d87772104756bf7bb6b7511b0964c63
leticialmbarros <armixletix@gmail.com>
2022-10-18 16:03:33 -0300

    renomeando documento

---
+++ condicionais.js
@@ -0,0 +1,16 @@
+
+/*true e false codificação binare 0 e 1
+
+10 > 5 = true
+10 < 5 = false
+
+const CamisetaRenanAzul = true
+const CamisetaAlemao = false 
+
+*/
+
+const numero = 10;
+
+const eNumeroPar = numero % 2;
+
+console.log(numeroPar);
