42e142ab28d5409a6b52d9160b2257eec622a9e4
leticialmbarros <armixletix@gmail.com>
2022-10-18 13:26:26 -0300

    primeiro desafio com gabarito e comentarios

--- primeiro-desafio.js
+++ primeiro-desafio.js
@@ -5,13 +5,24 @@ Você terá três variaveis, sendo elas:
 2 Valor Médio do combustivel gasto; 
 3- Distancia em KM;
 Imprima no console o valor que será gasto para realizar uma viagem.
-*/
 
+RESPOSTA:
 const precoCombustivel = 5.79;
-const KmPorLitro = 10;
-const distaciaEmKm = 100;
+const KmPorLitro = 12;
+const distaciaEmKm = 1580;
 
 const litrosConsumidos = distaciaEmKm / KmPorLitro;
 const valorGasto = litrosConsumidos * precoCombustivel;
 
 console.log(valorGasto);
+*/
+
+ 
+const precoCombustivel = 5.79;
+const KmPorLitro = 12;
+const distaciaEmKm = 1580;
+
+const litrosConsumidos = distaciaEmKm / KmPorLitro;
+const valorGasto = litrosConsumidos * precoCombustivel;
+
+console.log(valorGasto.toFixed(2)); //ARREDONDANDO VALORES - usa-se o .tofixed("aqui que será a quantidade de n após a casa decimal");
