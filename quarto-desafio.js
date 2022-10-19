a9d1abe22c4fc9a445da568c8e5c4c4578950b01
leticialmbarros <armixletix@gmail.com>
2022-10-18 23:25:14 -0300

    quarto desafio - icm

--- quarto-desafio.js
+++ quarto-desafio.js
@@ -11,3 +11,22 @@
     - Entre 30 e 40 Obeso;
     - Acima de 40 Obsesidade Grave;
 */
+
+const peso = 70
+const altura = 1.70
+const imc = peso / (altura * altura); //Math.pow(altura, 2)> biblioteca de funções matematicas para potencializar ou seja "tanto ao quadrado ou ao cubo e etc"
+console.log(imc); 
+
+if (imc > 18.5) {
+    console.log('Abaixo do Peso');
+} else if (imc >= 18.5 && imc < 25) {
+    console.log('Peso Normal');
+} else if (imc >= 25 && imc < 30) {
+    console.log('Acima do Peso');
+} else if(imc >= 30 && imc < 40){
+    console.log('Obeso')
+}
+
+
+
+// console é um objeto e o log é uma função
