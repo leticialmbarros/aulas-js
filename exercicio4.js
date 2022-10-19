96b66c8e507e5be14fef1645b6e856046c17c293
leticialmbarros <armixletix@gmail.com>
2022-10-19 18:51:38 -0300

    organizando o repositorio

--- exercicio4.js
+++ exercicio4.js
@@ -12,19 +12,21 @@
     - Acima de 40 Obsesidade Grave;
 */
 
-const peso = 70
-const altura = 1.70
+const peso = 63;
+const altura = 1.75;
 const imc = peso / (altura * altura); //Math.pow(altura, 2)> biblioteca de funções matematicas para potencializar ou seja "tanto ao quadrado ou ao cubo e etc"
 console.log(imc); 
 
-if (imc > 18.5) {
+if (imc < 18.5) {
     console.log('Abaixo do Peso');
 } else if (imc >= 18.5 && imc < 25) {
     console.log('Peso Normal');
 } else if (imc >= 25 && imc < 30) {
     console.log('Acima do Peso');
 } else if(imc >= 30 && imc < 40){
-    console.log('Obeso')
+    console.log('Obeso');
+} else{
+    console.log('Obesidade Grave');
 }
 
 
