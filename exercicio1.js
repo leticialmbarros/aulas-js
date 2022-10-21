26fb30e15ae5f934947cb61fffc92fba7d8be08f
leticialmbarros <armixletix@gmail.com>
2022-10-21 11:36:58 -0300

    exercicio 2 aplicando desconto e juros

---
+++ exercicio1.js
@@ -0,0 +1,18 @@
+// fazer uma função que escreve meu nome e verificar se sou maior de idade ou não:
+
+function escrevaMeuNome(nome) {
+   
+    console.log('Meu nome é '+ nome);
+}
+
+escrevaMeuNome('Leticia');
+
+function verificarIdade(idade){
+ if (idade >= 18){
+    console.log('Maior');
+ }else{
+    console.log('Menor');
+ }   
+} 
+
+verificarIdade('27');
