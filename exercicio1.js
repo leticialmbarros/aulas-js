5cad07c00b36de5e0e1fcb6529d37d7b81fba020
leticialmbarros <armixletix@gmail.com>
2022-10-21 15:52:24 -0300

    exercicio1

---
+++ exercicio1.js
@@ -0,0 +1,45 @@
+/*
+
+1) Crie uma classe para representar carros. 
+Os carros possuem uma marca, uma cor e um gasto médio de combustível por Kilômetro rodado.
+Crie um método que dado a quantidade de quilómetros e o preço do combustível nos dê o valor 
+gasto em reais para realizar este percurso.
+
+2) Crie uma classe para representar pessoas.
+Para cada pessoa teremos os atributos nome, peso e altura.
+As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
+Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor
+do seu IMC;
+
+*/
+
+class Pessoa {
+    nome;
+    idade;
+    anoDeNascimento;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2022 - idade;
+    }
+
+    descrever() {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);  
+    }
+}
+
+function compararPessoas(p1, p2) {
+    if (p1.idade > p2.idade) {
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);
+    } else if (p2.idade > p1.idade) {
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`);
+    }
+}
+
+const vitor = new Pessoa('Vitor', 25);
+const renan = new Pessoa('Renan', 30);
+
+compararPessoas(vitor, renan);
